97135f092e1dd4ba0d76a6babce236260edc2dbc
ShreshthaSingh011 <50359330+ShreshthaSingh011@users.noreply.github.com>
2023-07-26 14:44:23 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -4,7 +4,7 @@ from PIL import Image
 import pandas as pd
 import base64
 import matplotlib.pyplot as plt
-from bs4 import beautifulsoup4
+from bs4 import BeautifulSoup
 import requests
 import json
 import time
